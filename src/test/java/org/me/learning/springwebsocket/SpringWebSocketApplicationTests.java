61bfbd0573dddc676d3838802fd8e63a8d073266
mohamadalabsi <absimoda1@gmail.com>
2025-03-18 19:36:02 +0100

    Starting with WebSocket

---
+++ src/test/java/org/me/learning/springwebsocket/SpringWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
